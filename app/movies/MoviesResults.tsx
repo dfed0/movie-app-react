56b8876c76bbef168d8e04e38eba03cb9007e99c
dfed0 <denisfedorchuk2004@gmail.com>
2025-03-15 11:43:26 +0200

    first step working movie app

---
+++ app/movies/MoviesResults.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+export default function MoviesResults({ movies, options }: any) {
+  function getClassByRating(vote: any) {
+    if (vote >= 7) {
+      return 'green'
+    } else if (vote >= 4) {
+      return 'orange'
+    } else {
+      return 'red'
+    }
+  }
+  return (
+    <>
+      {movies.map((movie: any) => {
+        const { id, title, poster_path, vote_average, overview } = movie
+        return (
+          <div className="movie" key={id}>
+            <img src={IMG_PATH + poster_path} alt={title} />
+            <div className="movie-info">
+              <h3>{title}</h3>
+              <span className={getClassByRating(vote_average)}>
+                {vote_average.toFixed(1)}
+              </span>
+            </div>
+            <div className="overview">
+              <h1 id="overview-header">Overview</h1>
+              <p>{overview}</p>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,90 @@
-import Image from "next/image";
+// import Image from 'next/image'
+'use client'
+import { useEffect, useRef, useState } from 'react'
+import MoviesResults from './movies/MoviesResults'
+
+// const API_MOVIES: any = []
+
+// const API =
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWEwMDgzYTA1MWJlYjFlZDkyZjlhNTkwM2IyZGJkNSIsIm5iZiI6MTc0NDQ1MzI3My4yMjg5OTk5LCJzdWIiOiI2N2ZhM2U5OWVhODBkODUxNzU5OWYxMTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.kYdiKg80UYfT6OPhCg_0CNv27tM1Ey3rwICp7ypWSfg',
+  },
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [movies, setMovies] = useState<any[]>([])
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  const inputRef = useRef<HTMLInputElement>(null)
+  const mainRef = useRef<HTMLInputElement>(null)
+
+  function handleSubmit(e: any) {
+    e.preventDefault()
+    // console.log(e)
+    if (inputRef.current) {
+      const searchTerm = inputRef.current.value // Получаем значение из инпута
+      if (searchTerm && searchTerm !== '') {
+        getMovies(searchTerm)
+      }
+    }
+  }
+  function showMovies(movies: any) {
+    console.log(movies)
+    setMovies(movies.results)
+    // const { title, poster_path, vote_average, overview } = movie
+    // console.log(title, poster_path, vote_average, overview, movie)
+  }
+  function getMovies(url: any) {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?language=en-US&query="${url}"`,
+      options
+    )
+      .then((res) => res.json())
+      .then((data) => showMovies(data))
+      .catch((err) => console.error(err))
+  }
+
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+      options
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const sorted = data.results.sort(
+          (a: any, b: any) => b.popularity - a.popularity
+        )
+        setMovies(sorted)
+      })
+      .catch((err) => console.error(err))
+  }, [])
+  useEffect(() => {
+    console.log('OK', movies)
+  }, [movies])
+
+  return (
+    <>
+      <header>
+        <form id="form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="search"
+            className="search"
+            placeholder="Search"
+            ref={inputRef}
+          />
+        </form>
+      </header>
+      <main id="main" ref={mainRef}>
+        {movies.length > 0 ? (
+          <MoviesResults movies={movies} options={options} />
+        ) : (
+          ''
+        )}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    </>
+  )
 }
